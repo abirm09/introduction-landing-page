bfa61952c34ca4a2a9b75f5d5cb963b3f5b135e7
Md. Abir mahmud <abirmahmud5665@gmail.com>
2025-01-18 12:28:25 +0600

    done : Configure eslint, setup prettier, husky, lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,29 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next", "prettier", "eslint:recommended"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-console": "error",
+      "no-undef": "error",
+      "no-unused-expressions": "error",
+      "no-unreachable": "error",
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "react/react-in-jsx-scope": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
