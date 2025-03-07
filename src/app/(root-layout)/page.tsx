d1ca2c5bbfd6b0579a711e2678e6362d9915126e
MD Abir mahmud <abirmahmud5665@gmail.com>
2025-03-07 17:17:37 +0600

    done : changed color variables

--- src/app/(root-layout)/page.tsx
+++ src/app/(root-layout)/page.tsx
@@ -1,5 +1,14 @@
 const HomePage = async () => {
-  return <div></div>;
+  return (
+    <div>
+      <p className="bg-secondary text-secondary-foreground">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa debitis
+        sunt possimus ut, atque consequatur maxime beatae! Hic sit, deserunt
+        provident, tenetur veniam suscipit, molestiae obcaecati fuga quaerat
+        distinctio eius.
+      </p>
+    </div>
+  );
 };
 
 export default HomePage;
